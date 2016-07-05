81d04dc4f19f4004631d3a8b909fd511e85e9619
Mike Vrooman <hachima@comcast.net>
2016-07-04 23:58:10 -0700

    Better Harvester AI

--- main.js
+++ main.js
@@ -12,43 +12,40 @@ module.exports.loop = function () {
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
 
-    if (harvesters.length < 2) {
+    if (harvesters.length < 6) {
         var newName = Game.spawns.HomeSpawn.createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
 
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    console.log('Builders: ' + harvesters.length);
 
-    if (builders.length < 1) {
+    if (builders.length < 3) {
         var newName = Game.spawns.HomeSpawn.createCreep([WORK, CARRY, MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
 
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    console.log('Builders: ' + harvesters.length);
 
-    if (builders.length < 1) {
+    if (upgraders.length < 3) {
         var newName = Game.spawns.HomeSpawn.createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if (tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if (closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+    // var tower = Game.getObjectById('TOWER_ID');
+    // if (tower) {
+    //     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    //         filter: (structure) => structure.hits < structure.hitsMax
+    //     });
+    //     if (closestDamagedStructure) {
+    //         tower.repair(closestDamagedStructure);
+    //     }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
+    //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    //     if (closestHostile) {
+    //         tower.attack(closestHostile);
+    //     }
+    // }
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
